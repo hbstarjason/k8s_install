8aad716b50d52b80ef0a3757777c81c7b19bb071
hbstarjason <hbstarjason@gmail.com>
2019-05-27 13:48:32 +0800

    Update deploy-helm.sh

--- deploy-helm.sh
+++ deploy-helm.sh
@@ -2,7 +2,7 @@
 
 HELM_VERSION="v2.14.0"
 
-curl -sSL https://kubernetes-helm.storage.googleapis.com/helm-${HELM_VERSION}-linux-amd64.tar.gz-linux-amd64.tar.gz -o helm-linux-amd64.tar.gz
+curl -sSL https://kubernetes-helm.storage.googleapis.com/helm-${HELM_VERSION}-linux-amd64.tar.gz -o helm-linux-amd64.tar.gz
 
 tar zxvf helm-linux-amd64.tar.gz
 mv linux-amd64/helm /usr/local/bin/
