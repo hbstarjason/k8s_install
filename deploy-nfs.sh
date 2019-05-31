20106370cb722592ffd432a77ab07c117e14322e
hbstarjason <hbstarjason@gmail.com>
2019-05-31 11:56:34 +0800

    Create deploy-nfs.sh

---
+++ deploy-nfs.sh
@@ -0,0 +1,17 @@
+mkdir -p /data/nfs && chmod a+rw /data/nfs
+
+apt-get update && apt-get install -y nfs-kernel-server nfs-common
+
+cat << EOF >> /etc/exports
+/data/nfs *(rw,sync,no_subtree_check)
+EOF
+
+/etc/init.d/nfs-kernel-server restart
+
+mkdir -p /data/nfs-mount
+mount 172.17.0.17:/data/nfs  /data/nfs-mount
+df
+
+# umount /data/nfs-mount
+# ip addr > /data/nfs-mount/test.txt
+# cat /data/nfs/test.txt
