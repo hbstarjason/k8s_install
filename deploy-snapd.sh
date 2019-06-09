b98110fc26f21d2248e946ebf3ce630c2d9f274a
hbstarjason <hbstarjason@gmail.com>
2019-06-10 02:05:40 +0800

    Create deploy-snapd.sh

---
+++ deploy-snapd.sh
@@ -0,0 +1,37 @@
+# Ubuntu 14.04
+
+mv /etc/apt/sources.list /etc/apt/sources.list.bak
+
+cat > /etc/apt/sources.list << EOF 
+deb http://mirrors.aliyun.com/ubuntu/ trusty main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ trusty-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ trusty-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ trusty-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ trusty-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ trusty main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ trusty-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ trusty-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ trusty-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ trusty-backports main restricted universe multiverse
+EOF
+
+# https://docs.snapcraft.io/installing-snap-on-ubuntu
+apt-get update && apt-get install snapd
+
+# https://microk8s.io/
+sudo snap install microk8s --classic
+
+
+apt-get update && apt-get install -y apt-transport-https
+curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
+EOF
+
+apt-get update && apt-get install -y kubelet kubeadm kubectl
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.1.1/minikube-linux-amd64 && \
+chmod +x minikube && \
+sudo cp minikube /usr/local/bin/ && rm minikube
+minikube start --vm-driver=none
+minikube dashboard
