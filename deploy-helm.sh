754c2fb9b91d1b4c2d85814300be0114e514bb02
hbstarjason <hbstarjason@gmail.com>
2019-06-12 01:14:26 +0800

    Update deploy-helm.sh

--- deploy-helm.sh
+++ deploy-helm.sh
@@ -6,6 +6,8 @@ HELM_VERSION="v2.14.0"
 
 curl -sSL https://kubernetes-helm.storage.googleapis.com/helm-${HELM_VERSION}-linux-amd64.tar.gz -o helm-linux-amd64.tar.gz
 
+# wget -c https://kubernetes-helm.storage.googleapis.com/helm-v2.14.0-linux-amd64.tar.gz
+
 tar zxvf helm-linux-amd64.tar.gz
 mv linux-amd64/helm /usr/local/bin/
 rm -rf helm-linux-amd64.tar.gz linux-amd64
@@ -14,7 +16,7 @@ rm -rf helm-linux-amd64.tar.gz linux-amd64
 kubectl create -f https://raw.githubusercontent.com/hbstarjason/k8s_install/master/helm-admin.yaml
 helm init
 
-# helm init --service-account tiller --upgrade -i registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:v2.14.0 --stable-repo-url https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts
+# helm init --service-account helm-admin --upgrade -i registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:v2.14.0 --stable-repo-url https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts
 
 # update repo
 helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com/
