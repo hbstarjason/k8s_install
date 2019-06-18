d97978d4b1514d7200b489adcc2eeac3d8575930
hbstarjason <hbstarjason@gmail.com>
2019-06-18 16:02:37 +0800

    Update deploy-helm.sh

--- deploy-helm.sh
+++ deploy-helm.sh
@@ -2,6 +2,8 @@
 
 # wget https://raw.githubusercontent.com/hbstarjason/k8s_install/master/deploy-helm.sh 
 
+# curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
+
 HELM_VERSION="v2.14.0"
 
 curl -sSL https://kubernetes-helm.storage.googleapis.com/helm-${HELM_VERSION}-linux-amd64.tar.gz -o helm-linux-amd64.tar.gz
