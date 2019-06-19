f55b25f687a7cc181e9a4c76f33b87ace59260f8
hbstarjason <hbstarjason@gmail.com>
2019-06-19 14:30:25 +0800

    Update install.sh

--- spinnaker/install.sh
+++ spinnaker/install.sh
@@ -1,4 +1,4 @@
-# wget 
+# wget https://raw.githubusercontent.com/hbstarjason/k8s_install/master/spinnaker/install.sh
 
 kubectl apply -f  https://raw.githubusercontent.com/hbstarjason/k8s_install/master/spinnaker/minio-pv.yml
 kubectl apply -f  https://raw.githubusercontent.com/hbstarjason/k8s_install/master/spinnaker/quick-install-spinnaker.yml
