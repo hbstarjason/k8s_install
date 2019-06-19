ac2eba41dc75d6953938546b3f79d2fef155bb11
hbstarjason <hbstarjason@gmail.com>
2019-06-19 14:15:43 +0800

    Create install.sh

---
+++ spinnaker/install.sh
@@ -0,0 +1,4 @@
+# wget 
+
+kubectl apply -f  https://raw.githubusercontent.com/hbstarjason/k8s_install/master/spinnaker/minio-pv.yml
+kubectl apply -f  https://raw.githubusercontent.com/hbstarjason/k8s_install/master/spinnaker/quick-install-spinnaker.yml
