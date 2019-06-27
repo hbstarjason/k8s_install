70c3a57dcca024e3fd4eeb611f09e8fc01fb4504
hbstarjason <hbstarjason@gmail.com>
2019-06-27 10:35:08 +0800

    Update install.sh

--- spinnaker/install.sh
+++ spinnaker/install.sh
@@ -1,4 +1,48 @@
-# wget https://raw.githubusercontent.com/hbstarjason/k8s_install/master/spinnaker/install.sh
+###### https://www.spinnaker.io/setup/install/halyard/
+curl -O https://raw.githubusercontent.com/spinnaker/halyard/master/install/debian/InstallHalyard.sh
+sudo bash InstallHalyard.sh
+hal -v
+sudo update-halyard
+
+mkdir ~/.hal
+docker run -p 8084:8084 -p 9000:9000 \
+    --name halyard --rm \
+    -v ~/.hal:/home/spinnaker/.hal \
+    -d \
+    gcr.io/spinnaker-marketplace/halyard:stable
+
+
+###### https://github.com/aws-samples/aws-deploy-spinnaker-halyard/blob/master/scripts/start_hal_daemon.sh
+docker run -p 8084:8084 -p 9000:9000 \
+    --name halyard --rm \
+    -v $(pwd)/.hal:/home/spinnaker/.hal \
+    -v ~/.kube:/home/spinnaker/.kube \
+    -v ~/.aws:/home/spinnaker/.aws \
+    -d \
+    gcr.io/spinnaker-marketplace/halyard:stable
+
+docker exec -it halyard bash
+source <(hal --print-bash-completion)
+
+###### https://blogs.oracle.com/developers/install-spinnaker-with-halyard-on-kubernetes
+mkdir halyard && chmod 747 halyard
+mkdir k8s && cp $KUBECONFIG k8s/config && chmod 755 k8s/config
+
+docker run -p 8084:8084 -p 9000:9000 \
+    --name halyard -d \
+    -v /sandbox/halyard:/home/spinnaker/.hal \
+    -v /sandbox/k8s:/home/spinnaker/k8s \
+    -e http_proxy=http://<proxy_host>:<proxy_port> \
+    -e https_proxy=https://<proxy_host>:<proxy_port> \    
+    -e JAVA_OPTS="-Dhttps.proxyHost=<proxy_host> -Dhttps.proxyPort=<proxy_port>" \
+    -e KUBECONFIG=/home/spinnaker/k8s/config \
+    gcr.io/spinnaker-marketplace/halyard:stable
+
+docker exec -it halyard bash
+kubectl get pods -n spinnaker
+
+
+###### wget https://raw.githubusercontent.com/hbstarjason/k8s_install/master/spinnaker/install.sh
 
 kubectl apply -f  https://raw.githubusercontent.com/hbstarjason/k8s_install/master/spinnaker/minio-pv.yml
 kubectl apply -f  https://raw.githubusercontent.com/hbstarjason/k8s_install/master/spinnaker/quick-install-spinnaker.yml
